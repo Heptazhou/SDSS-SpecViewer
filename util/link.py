3568b4a5c7f070b977b24e954e816d26dbf8e824
yorkphall <yorkphall@gmail.com>
2025-11-03 18:21:58 -0500

    Update link.py from dr18 to dr19

--- util/link.py
+++ util/link.py
@@ -19,15 +19,15 @@ def object_links(ra: float, dec: float) -> Iterable[str]:
 	yield f"Legacy Survey Viewer: {url}"
 
 	arg = dict(opt="G", scale="0.0420", width=1280, height=1280, ra=ra, dec=dec)
-	url = "https://skyserver.sdss.org/dr18/SkyServerWS/ImgCutout/getjpeg" + f(arg)
+	url = "https://skyserver.sdss.org/dr19/SkyServerWS/ImgCutout/getjpeg" + f(arg)
 	yield f"SDSS Image Cutout: {url}"
 
 	arg = dict(scale="0.09", ra=ra, dec=dec)
-	url = "https://skyserver.sdss.org/dr18/VisualTools/navi" + f(arg)
+	url = "https://skyserver.sdss.org/dr19/VisualTools/navi" + f(arg)
 	yield f"SDSS Explore Navi: {url}"
 
 	arg = dict(ra=ra, dec=dec)
-	url = "https://skyserver.sdss.org/dr18/VisualTools/explore/summary" + f(arg)
+	url = "https://skyserver.sdss.org/dr19/VisualTools/explore/summary" + f(arg)
 	yield f"SDSS Explore Summary: {url}"
 
 	arg = dict(Radius="6.00", Coord=f"{ra}d{dec}d")
